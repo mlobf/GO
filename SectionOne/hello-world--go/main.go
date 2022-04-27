99041682017cdaaf1e74ba58d154d50d42066cf0
mlobf <marcoslemeborbafilho@gmail.com>
2022-04-27 19:52:21 -0300

    How to build a exe from a GO File.

--- SectionOne/hello-world--go/main.go
+++ SectionOne/hello-world--go/main.go
@@ -5,18 +5,37 @@
 	go mod init theNameOfTheApp
 */
 
+// To build a exe file using GO .....
+// 	go build -o eliza main.go
+
 package main
 
 // Testing the git
 
 import (
+	"bufio"
 	"fmt"
 	"myapp/doctor"
+	"os"
+	"strings"
 )
 
 func main() {
 
+	reader := bufio.NewReader(os.Stdin)
+
 	whatToSay := doctor.Intro()
 	fmt.Println(whatToSay)
 
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response((userInput)))
+		}
+	}
 }
