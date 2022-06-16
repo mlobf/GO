901445c54fbdb4a78dd34f4f0560289aae77b6a1
mlobf <marcoslemeborbafilho@gmail.com>
2022-06-16 14:03:13 -0300

    Add sum all in array

--- goFunctions/main.go
+++ goFunctions/main.go
@@ -11,13 +11,28 @@ import (
 
 func main() {
 
-	f := addTwoNumbers(2, 2)
-	fmt.Println(f)
-
+	//f := addTwoNumbers(2, 2)
+	//fmt.Println(f)
 	fmt.Println("Lets Go learn how to create some functions in Go")
 
+	s := sumMany(1, 2, 2, 3, 4, 4, 5, 90)
+	fmt.Println(s)
 }
 
+// Sum two numbers
 func addTwoNumbers(numOne, numTwo int) int {
 	return numOne + numTwo
+}
+
+// Sum a array of numbers
+// variatic params
+func sumMany(nums ...int) int {
+	total := 0
+
+	for _, x := range nums {
+		total = total + x
+	}
+
+	return total
+
 }
