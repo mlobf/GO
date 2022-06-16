fb478817ddac3bff7f75a7ea6a5932151e3b1fdf
mlobf <marcoslemeborbafilho@gmail.com>
2022-06-15 23:49:07 -0300

    Using and creating function in GO

---
+++ goFunctions/main.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+    Now lets Go Learn function in 
+        Go.
+*/
+
+
+import("fmt")
+
+func main() {
+
+    f :=addTwoNumbers(2,2)
+    fmt.Println(f)
+
+
+    fmt.Println("Lets Go learn how to create some functions in Go")
+
+}
+
+func addTwoNumbers(numOne, numTwo int) int {
+    return numOne + numTwo
+}
+
+
