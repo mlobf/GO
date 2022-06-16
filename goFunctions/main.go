c27c2da0286272b2d05372fc966813dc7b976407
mlobf <marcoslemeborbafilho@gmail.com>
2022-06-16 14:28:01 -0300

    Using a func as a method of a struct

--- goFunctions/main.go
+++ goFunctions/main.go
@@ -9,6 +9,24 @@ import (
 	"fmt"
 )
 
+// Time to build some structs
+
+type Animal struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
+
+func (a *Animal) Says() {
+	fmt.Printf("A %s says %s", a.Name, a.Sound)
+	fmt.Println("")
+}
+
+func (a *Animal) HowManyLegs() {
+	fmt.Printf("This buddy %s has a Total of %d legs.", a.Name, a.NumberOfLegs)
+	fmt.Println("")
+}
+
 func main() {
 
 	//f := addTwoNumbers(2, 2)
@@ -17,6 +35,21 @@ func main() {
 
 	s := sumMany(1, 2, 2, 3, 4, 4, 5, 90)
 	fmt.Println(s)
+
+	var dog Animal
+	dog.Name = "dog"
+	dog.Sound = "au au"
+	dog.NumberOfLegs = 4
+	dog.Says()
+	dog.HowManyLegs()
+
+	var cat Animal
+	cat.Name = "Tiao"
+	cat.Sound = "Miau"
+	dog.NumberOfLegs = 4
+	cat.Says()
+	cat.HowManyLegs()
+
 }
 
 // Sum two numbers
