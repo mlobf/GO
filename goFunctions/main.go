a9daa07b98bb2fce953d89ab45789ff27ecec2b6
mlobf <marcoslemeborbafilho@gmail.com>
2022-06-16 13:07:10 -0300

    add some configs at codium

--- goFunctions/main.go
+++ goFunctions/main.go
@@ -1,25 +1,23 @@
 package main
 
 /*
-    Now lets Go Learn function in 
-        Go.
+Now lets Go Learn function in
+Go.
 */
 
-
-import("fmt")
+import (
+	"fmt"
+)
 
 func main() {
 
-    f :=addTwoNumbers(2,2)
-    fmt.Println(f)
+	f := addTwoNumbers(2, 2)
+	fmt.Println(f)
 
-
-    fmt.Println("Lets Go learn how to create some functions in Go")
+	fmt.Println("Lets Go learn how to create some functions in Go")
 
 }
 
 func addTwoNumbers(numOne, numTwo int) int {
-    return numOne + numTwo
+	return numOne + numTwo
 }
-
-
--- goMaps/main.go
+++ goMaps/main.go
@@ -1,44 +1,44 @@
 package main
+
 /*
-    Maps are keys and values.
-    in Python there are know as Dict
-    in Js there are know as Objects
+Maps are keys and values.
+in Python there are know as Dict
+in Js there are know as Objects
 */
 
-import("fmt")
+import (
+	"fmt"
+)
 
+func main() {
 
-func main(){
+	inMap := make(map[string]int)
 
-    inMap:=make(map[string]int)
+	inMap["one"] = 1
+	inMap["two"] = 2
+	inMap["three"] = 3
+	inMap["four"] = 4
+	inMap["five"] = 5
 
-    inMap["one"] = 1
-    inMap["two"] = 2
-    inMap["three"] = 3
-    inMap["four"] = 4
-    inMap["five"] = 5
+	for key, value := range inMap {
+		fmt.Println("This is a key => ", key, " And this is the value ->", value)
+	}
 
-    for key, value :=  range inMap {
-        fmt.Println("This is a key => ",key, " And this is the value ->",value)
-    }
+	// To test if some element is in a map
+	el, ok := inMap["four"]
 
-    // To test if some element is in a map
-    el, ok:=inMap["four"]
+	if ok {
+		fmt.Println(el, " is in map")
+	} else {
+		fmt.Println(el, " is not in map")
+	}
 
-    if ok {
-    fmt.Println(el," is in map")  
-    }else{
-    fmt.Println(el," is not in map")  
-    }
+	// To delete in map
+	delete(inMap, "four")
 
-    // To delete in map
-    delete(inMap,"four")
+	// To change some value in a element
+	inMap["two"] = 4
 
-
-    // To change some value in a element
-    inMap["two"]=4
-
-    // To print all elements in a map
-    fmt.Println("Oi, este é o meu map => ", inMap)
+	// To print all elements in a map
+	fmt.Println("Oi, este é o meu map => ", inMap)
 }
-
